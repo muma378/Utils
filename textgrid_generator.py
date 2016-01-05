75763618ca96f5db5ef65e4ffc2b779772b17db3
xiaoyang <muma.378@163.com>
2016-01-05 17:42:30 +0800

    [FIX] fixed the header for textgrid

--- textgrid_generator.py
+++ textgrid_generator.py
@@ -1,27 +1,30 @@
+# -*- coding: utf-8 -*-
+
 # TextGridGenerator.py - usage: python TextGridGenerator.py configure.txt
 # to parse the configure.txt and translate it into textgrid files
 # author: xiao yang <xiaoyang0117@gmail.com>
 # date: 2015.12.28
+# the format of the text to parse is below:
+# http://crowdfile.blob.core.chinacloudapi.cn/cutted-wav-blob/20150810_225453_52_193.63_194.975.wav	1	0.00785	1.44	我有去问我一个	1.44
 
 import os
 import sys
 import re
 import subprocess
 
-import collections
 
 APPENDIX = ".TextGrid"
 TEMPLATE_HEADER = """File type = "ooTextFile"
 Object class = "TextGrid"
-"""
 
-TEMPLATE_ITEM = """
 xmin = {global_xmin}
 xmax = {global_xmax}
 tiers? <exists>
-size = 1
+size = 2
 item []:
-	item [1]:
+"""
+
+TEMPLATE_ITEM = """	item [{item_index}]:
 		class = "IntervalTier"
 		name = "text"
 		xmin = {global_xmin}
@@ -41,8 +44,13 @@ def parse_file(src, items):
 		for line in f:
 			parse_line(line, items)
 
-
-
+# to convert lines in the config into a dict with keys described below
+# items = { 
+# 	'20150810_225453': [
+# 		{ 'slice': 52, 'xmin': 193.63785, 'xmax': 195.07, 'text': u'我有去问我一个'},
+# 		{ ... }
+# 		] 
+# 	}
 def parse_line(line, items):
 	columns = line.split('\t')
 	if columns[1] == '1':
@@ -57,23 +65,51 @@ def parse_line(line, items):
 				items[groups['name']].append(info)
 			except KeyError, e:
 				items[groups['name']] = [info]
-		
+
+def generate_interval(aslice, interval_index, text=''):
+	return TEMPLATE_INTERVALS.format(interval_index=interval_index, local_xmin=aslice['xmin'], local_xmax=aslice['xmax'], text=text)
+
+def generate_output(filled_slices):
+	intervals_size = len(filled_slices)
+	global_xmin = filled_slices[0]['xmin']
+	global_xmax = filled_slices[intervals_size-1]['xmax']
+	
+	output = TEMPLATE_HEADER.format(**locals())
+	# first time: fill it with empty text
+	item_index = 1
+	output += TEMPLATE_ITEM.format(**locals())
+	for i, aslice in enumerate(filled_slices, start=1):
+		output += generate_interval(aslice, i)
+
+	# second time: fill it with real text
+	item_index = 2
+	output += TEMPLATE_ITEM.format(**locals())
+	for i, aslice in enumerate(filled_slices, start=1):
+		output += generate_interval(aslice, i, text=aslice['text'])
+	return output
+
+# to fill 'gaps' in the list of slices
+# gaps means the values of xmax and xmin in continus slices are not the same
+def prefill_items(slices):
+	ordered_slices = sorted(slices, key=lambda x:x['slice'])
+	previous_xmax = 0
+	filled_slices = []
+	for aslice in ordered_slices:
+		if previous_xmax != aslice['xmin']:
+			filled_slices.append({'slice': aslice['slice']-1, 'xmin': previous_xmax, 'xmax': aslice['xmin'], 'text': ''})
+		filled_slices.append(aslice)
+		previous_xmax = aslice['xmax']
+	return filled_slices
+
 
 if __name__ == '__main__':
 	items = {}
 	parse_file(sys.argv[1], items)
 	directory_name = sys.argv[1].split('.')[0]
 	subprocess.call('mkdir '+directory_name, shell=True)
-	ordered = collections.OrderedDict(sorted(items.items()))
-	for k, v in ordered.iteritems():
-		dst = directory_name + '/' + k + '.textgrid'
+	# ordered = collections.OrderedDict(sorted(items.items()))
+	for filename, slices in items.items():
+		dst = directory_name + '/' + filename + '.textgrid'
 		with open(dst, "w") as f:
-			f.write(TEMPLATE_HEADER)
-			v = sorted(v, key=lambda x:x['slice'])
-			intervals_size = len(v)
-			head = TEMPLATE_ITEM.format(global_xmin=v[0]['xmin'], global_xmax=v[intervals_size-1]['xmax'], intervals_size=intervals_size)
-			body = head
-			for i, s in enumerate(v):
-				body += TEMPLATE_INTERVALS.format(interval_index=i, local_xmin=s['xmin'], local_xmax=s['xmax'], text=s['text'])
-			f.write(body)
-
+			slices =prefill_items(slices)
+			f.write(generate_output(slices))
--- uniq_multi_files.sh
+++ uniq_multi_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# uniq_multi_files.sh - usage: ./uniq_multi_files.sh file1 file2 ...
+# to make the lines within the files unique, remove repeated lines in the latter file
+# author: xiao yang <xiaoyang0117@gmail.com>
+# date: 2015.12.29
 
 MERGEFILE='merge.txt'
 UNIQ_MERGEFILE='uniq_merge.txt'
@@ -7,7 +11,7 @@ echo '' > $MERGEFILE
 
 for i in $@;do
     if [ -e ${i} ];then
-        sort $i | uniq | sed "s/.*/${i}  &/" >> $MERGEFILE 
+        sed '/^$/d' ${i} | sort | uniq | sed "s/.*/${i}  &/" >> $MERGEFILE 
     else
         echo 'Can not find the file '
         echo $i
@@ -21,7 +25,7 @@ for i in $@;do
     awk -v filename=${i} '
         BEGIN { FS=" " };
         filename==$1 {$1=""; print $0;};
-        ' $UNIQ_MERGEFILE | sed 's/^ //' > ${i}".backup" 
+        ' $UNIQ_MERGEFILE | sed 's/^ //' > ${i}".uniq" 
 done
 
 
