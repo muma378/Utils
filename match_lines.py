1abb47e659fbb883766c57593f3989b9a81ff0da
xiaoyang <muma.378@163.com>
2016-01-28 15:27:20 +0800

    [FIX] add entry before calling climb()

--- match_lines.py
+++ match_lines.py
@@ -4,7 +4,7 @@ import sys
 import dictionary
 
 LANGUAGE = dictionary.KR
-HEADER = "Extracted Lines For {key}"
+HEADER = "Extracted Lines For {key}\n"
 
 # unable to find the leaf finally
 class AstaryException(Exception):
@@ -31,34 +31,39 @@ def branching(tree, word, leaf):
 def climb(tree, path):
 	branch = tree
 	try:
-		for i, milestone in enumerate(path):
+		for i, milestone in enumerate(path, start=1):
 			branch = branch[milestone]
 	except KeyError, e:
 		if not branch.get(None):
 			raise AstaryException('Unable to find the leaf.')
-	path = path[i:]
+	# arrives at the end
 	return branch[None]
 
 # extracts lines matched the tree in to the dict extracted
 def extract_matched(filename, tree, extracted):
 	with open(filename, 'r') as f:
 		for line in f:
-			path = line
-			while len(path):	# traversed the path
-				try:
-					key = climb(tree, path)
-					extracted.setdefault(key, []).append(line)
-				except AstaryException, e:
-					pass
-	import pdb;pdb.set_trace()
+			i = 0
+			while i < len(line):
+				if tree.get(line[i]):		# the entry
+					try:
+						key = climb(tree, line[i:])
+						extracted.setdefault(key, []).append(line)
+						i += len(key)
+					except AstaryException, e:
+						i += 1
+				else:
+					i += 1
+				
 	return extracted
 
 def write(filename, extracted):
-	with open(filename, 'a') as f:
+	with open(filename, 'w') as f:
 		for key, lines in extracted.items():
 			f.write(HEADER.format(key=key.capitalize()))
 			for line in lines:
 				f.write(line)
+			f.write("\n\n")
 	print("Writes finished.")
 
 
@@ -67,8 +72,10 @@ def main():
 	tree = cultivate(LANGUAGE)
 	extracted = {}
 	for filename in os.listdir(src_root):
-		extract_matched(os.path.join(src_root, filename), tree, extracted)
-	write('matched.txt', extracted)
+		if filename.endswith('.txt'):
+			extract_matched(os.path.join(src_root, filename), tree, extracted)
+	dst_file = os.path.basename(src_root)
+	write(dst_file+'.txt', extracted)
 
 
 if __name__ == '__main__':
