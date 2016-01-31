[FIX] add computation using least squares

least_squares.py
@@ -2,9 +2,12 @@ import os
 import sys
 import re
 import csv
+import copy
 import math
 import numpy as np
 # import matplotlib.pyplot as plt
+
+TITLE = []
 
 try:
 	import math_settings as ms
@@ -15,13 +18,15 @@ except ImportError, e:
 
 # retrieve specified columns in the csvfile
 def retrive_data(filename, columns):
+	global TITLE
 	if not filename.endswith(ms.CSV_SUFFIX):
 		print("Warning: specified filename is detected not ending with 'csv', please check if it has a correct format")
 	
 	data = []
 	with open(filename, 'rb') as csvfile:
 		data_reader = csv.reader(csvfile, delimiter=',')
-		field_title = next(data_reader)
+		titles = next(data_reader)
+		TITLE = [ titles[i] for i in columns ]
 		for row in data_reader:
 			data.append([ float(row[i]) for i in columns ])
 	return data
@@ -80,9 +85,48 @@ def compute(data):
 	alpha_array = np.ones((1, len(y_array)))
 	mat = np.vstack((alpha_array, np.array(data)))
 
-	beta = np.dot(np.linalg.inv(mat.dot(mat.T)), mat.dot(y_array.T))
-	return beta
+	# core
+	coefficient = np.dot(np.linalg.inv(mat.dot(mat.T)), mat.dot(y_array.T))
+	return coefficient
 
+def fit_test(coefficient, data):
+	data_b = copy.copy(data)
+
+	y_real = np.array(data_b.pop(ms.Y_COL))
+	x_real = np.vstack((np.ones(len(y_real)), np.array(data_b)))
+	y_est = x_real.T.dot(coefficient)
+
+	ess = np.sum(np.square(y_real-y_est))
+	rss = np.sum(np.square(y_est-np.average(y_real)))
+	tss = ess + rss
+
+	N = len(y_real)		# observasitions number
+	K = len(data) - 1	# 
+
+	global TITLE
+	std_err = np.sqrt(np.linalg.inv(x_real.dot(x_real.T))*(ess/(N-K-1))).diagonal()
+	variables = [
+		("Variables", TITLE),
+		("Coefficient", coefficient.astype('str')),
+		("Std. Error", std_err.astype('str')),
+		("T-statics", (coefficient/std_err).astype('str')),
+	]
+	for key, vals in variables:
+		print("%s\t\t:%s" % (key, '  '.join(vals)))
+	
+	fit_description = {
+		'R-squared         ': rss/tss,
+		'Adjsuted R-squared': 1-((ess/tss)*(N-1)/(N-K-1)),
+		'S.E. of regression': math.sqrt(ess/(N-K-1)),
+		'S.D. dependent var': math.sqrt(tss/(N-1)),
+		'Sum squared residua': ess,
+		'Mean dependent var': np.mean(y_real),
+		'F-statics         ': (rss/2)/(ess/(N-K-1)),
+	}
+
+	for key, val in fit_description.items():
+		print("%s\t\t:%f" % (key, val))
+	print("Processing Finished")
 
 def main():
 	try:
@@ -106,8 +150,8 @@ def main():
 		fns.append(fn)
 
 	mapped_data = maping(data, len(columns), fns)
-	compute(mapped_data)
-
+	coefficient = compute(copy.copy(mapped_data))
+	fit_test(coefficient, copy.copy(mapped_data))
 
 
 if __name__ == '__main__':