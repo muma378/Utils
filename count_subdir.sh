aa1e28081f8319052819ccac57e5eb1296fbb4ff
xiaoyang <muma.378@163.com>
2016-02-29 09:48:21 +0800

    [ADD] count files in a specified directory

--- count_subdir.sh
+++ count_subdir.sh
@@ -1,1 +1,1 @@
-#!/bin/sh$ROOT_DIR=$1dirlist=(`find $ROOT_DIR -type d -regex '.*/[A-Z][a-z]*/[a-z]*$'`)for dir in ${dirlist[@]};do	find $dir -type f -regex '.*\.txt' | count -l	echo `basename dir`':'$numdone
+#!/bin/sh#run the following cmd before the sh#sed -i -e 's/\r\n/\n/g' count_subdir.shROOT_DIR='/cygdrive/g/20160226'dirlist=(`find $ROOT_DIR -type d -regex '.*/[A-Z][a-z]*/[a-z]*$'`)for dir in ${dirlist[@]};do	num=`find $dir -type f -regex '.*\.txt' | wc -l`	echo ${dir}': '$num >> info.txtdonesed -i -e "s:$ROOT_DIR:\.:g" info.txt
