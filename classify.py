5d691c60770a6e34eeef54602a564d42cc3c6778
xiaoyang <muma.378@163.com>
2016-03-30 14:55:55 +0800

    [FIX] chage coding to gb2312

--- classify.py
+++ classify.py
@@ -5,7 +5,9 @@
 # date: 2016.Mar.30
 
 import os
+import sys
 import json
+import shutil
 
 
 FILENAME_KEY = u'文件名'
@@ -24,41 +26,54 @@ DIRS_STRUCT = {
 DIRS_KEYS_SEQ = (CARD_CARVE_TYPE, CARD_DATE_TYPE)
 
 VALUE_DIRNAME_MAP = {
-	u"无": u"有日期",
-	u"有效日期": u"无日期",
+	u"无": u"无日期",
+	u"有效日期": u"有日期",
+	u"制卡日期": u"有日期",
+	u"发卡日期": u"有日期",
+	u"不确定": u"有日期",
 	u"浮凸": u"浮凸",
 	u"平面": u"平面"
 }
+
+for key, val in VALUE_DIRNAME_MAP.items():
+	VALUE_DIRNAME_MAP[key] = val.encode('gb2312')
 
 def classify(info_dict, root):
 	filelist = os.listdir(root)
 
 	for filename in filelist:
-		info = info_dict[filename]
+		try:
+			info = info_dict[filename]
+		except KeyError, e:
+			print filename.decode('gb2312') + u" is not in the list"
+			continue
+
 		try:
 			filepath = root
 			for dir_key in DIRS_KEYS_SEQ:
-				filepath = os.path.join(filepath, VALUE_DIRNAME_MAP[info[dir_key]])
+				dirname = VALUE_DIRNAME_MAP[info[dir_key]]
+				filepath = os.path.join(filepath, dirname)
 		except KeyError, e:
-			print u"unable to recognize info for file: " + filename
+			# import pdb;pdb.set_trace()
+			print u"unable to recognize info " + info[dir_key] + u" for file: " + filename
 			continue
 
 		if os.path.exists(filepath):
 			filepath = os.path.join(filepath, filename)
-			os.move(os.path.join(root, filename), filepath)
+			shutil.move(os.path.join(root, filename), filepath)
 		else:
-			print u"path " + filepath + u" is not exist"
+			print u"path " + filepath.decode('gb2312') + u" is not exist"
 			continue
 
 
 def construct_dirs(root, dirs_struct):
 	for sub_dir in traverse_dict(dirs_struct):
-		dirs = os.path.join(root, sub_dir)	
-		os.makedirs(dirs)
+		dirs = os.path.join(root, sub_dir.encode('gb2312'))
+		if not os.path.exists(dirs):	
+			os.makedirs(dirs)
 
 def traverse_dict(dirs_struct):
 	assert(type(dirs_struct) == dict)
-	sub_dirs = []
 	for parent, child in dirs_struct.items():
 		if not child:
 			yield parent
