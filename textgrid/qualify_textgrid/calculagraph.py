315d2cb9c3e9f37aa7999aec8e99f19327d1f2b7
xiaoyang <muma.378@163.com>
2016-04-21 16:37:46 +0800

    [FIX] refactor classes of calculagraph

--- textgrid/qualify_textgrid/calculagraph.py
+++ textgrid/qualify_textgrid/calculagraph.py
@@ -4,25 +4,55 @@
 # author: xiao yang <xiaoyang0117@gmail.com>
 # date: 2016.April.14
 
-
+import os
 import re
+import sys
+import base
+import operator
 from settings import logger
 import settings
 
-class Calculagraph(object):
+
+class BaseCalculagraph(base.BaseEvaluator):
 	TIME_UNIT = {
 		's':(1, u'秒'),
 		'm':(60.0, u'分'),
 		'h':(3600.0, u'小时')
 	}
+	def __init__(self):
+		super(BaseCalculagraph, self).__init__()
+		self.errors = []
+		self.msg=u"{duration_display}{unit_display}" + settings.LINESEP
 
+	def measure(self, intervals):
+		raise NotImplementedError
+
+	def output_duration(self, fd=sys.stdout, unit='s'):
+		raise NotImplementedError
+
+	def get_unit_display(self, unit):
+		try:
+			divider, unit_display = self.TIME_UNIT[unit]
+		except KeyError, e:
+			logger.error('unknown choice "%s" for unit' % unit)
+			raise e
+		return divider, unit_display
+
+	def output(self, fd, duration, msg=None, unit='s', **kwargs):
+		divider, unit_display = self.get_unit_display(unit)
+		if not msg:
+			msg = self.msg
+		fd.write(msg.format(duration_display=duration/divider, unit_display=unit_display, **kwargs).encode(settings.ENCODING))
+
+
+class PatternCalculagraph(BaseCalculagraph):
 	DEFAULT_PATTERN = re.compile('.*', re.UNICODE)
 
 	"""calculate duration if it matches the pattern"""
 	def __init__(self, match_parser=None):
-		super(Calculagraph, self).__init__()
+		super(PatternCalculagraph, self).__init__()
 		self.match_parser = match_parser if match_parser else self.DEFAULT_PATTERN	# matchs all by default
-		self.errors = []
+		self.msg = u"文件有效时长为{duration_display}{unit_display}" + settings.LINESEP
 
 	def measure(self, intervals):
 		self.duration = 0.0
@@ -38,31 +68,21 @@ class Calculagraph(object):
 			except AttributeError, e:
 				logger.error("unable to parse the text: %s at line %d" % (interval[settings.INTERVAL_KEY_TEXT], interval[settings.INTERVAL_KEY_LINENO]))
 				continue 
-				
-		return self.duration
+		return self	
+		# return self.duration
 
-	def get_unit_display(self, unit):
-		try:
-			divider, unit_display = self.TIME_UNIT[unit]
-		except KeyError, e:
-			logger.error('unknown choice "%s" for unit' % unit)
-			raise e
-		return divider, unit_display
-
-	def output_duration(self, unit='s'):
-		output = []
-		divider, unit_display = self.get_unit_display(unit)
-		output.append(u"有效时长为%f%s" % (self.duration/divider, unit_display))
-		return output
+	def output_duration(self, fd=sys.stdout, unit='s'):
+		self.output(fd, self.duration, self.msg, unit)
 
 
-class CategoricalCalculagraph(Calculagraph):
+class CategoricalCalculagraph(BaseCalculagraph):
 	"""calculate duration for different categories respectively"""
 	def __init__(self, category_parser, pattern_key, category_display=None):
 		super(CategoricalCalculagraph, self).__init__()
 		self.category_parser = category_parser
 		self.pattern_key = pattern_key
 		self.category_display = category_display if category_display else settings.MARKS_MEANING
+		self.msg = u'{category}时长为{duration_display}{unit_display}' + settings.LINESEP	# extra argument is needed
 
 	def measure(self, intervals):
 		self.assorted = {}
@@ -75,21 +95,47 @@ class CategoricalCalculagraph(Calculagraph):
 				else:
 					self.assorted[category] += time_len
 			except KeyError, e:
-				self.assorted[category] = time_len
+				self.assorted[category] = time_len		# new category
 			except AttributeError, e:
 				logger.error("unable to parse the text: %s at line %d" % (interval[settings.INTERVAL_KEY_TEXT], interval[settings.INTERVAL_KEY_LINENO]))
 				continue
+		self.duration = self.assorted	# adaptor
+		return self
 
-		# duration for all category
-		return self.assorted
-
-	def output_duration(self, unit='s'):
-		output = []
-		divider, unit_display = self.get_unit_display(unit)
+	def output_duration(self, fd=sys.stdout, unit='s'):
 		try:
-			for key, val in self.assorted.items():
-				output.append(u'%s时长为%f%s' % (self.category_display[key], val/divider, unit_display))
+			for key, val in self.duration.items():
+				self.output(fd, val, self.msg, unit, category=self.category_display[key])	
 		except KeyError, e:
 			logger.error('marks included are not supported')
 
-		return output
+def dict_add(d1, d2):
+	temp_dict = d2.copy()
+	for k in d1.keys():
+		temp_dict[k] = d1[k] + d2.get(k, 0)
+	return temp_dict
+
+class OverallCalculagraph(BaseCalculagraph):
+	"""calculate the overall duration for all files listed"""
+	def __init__(self, concrete_cal):
+		super(OverallCalculagraph, self).__init__()
+		self.add = operator.add  	# use + for most cases
+		if type(concrete_cal).__name__ == "PatternCalculagraph":
+			self.accum = 0
+		elif type(concrete_cal).__name__ == "CategoricalCalculagraph":
+			self.accum = {}
+			self.add= dict_add
+		else:
+			raise NotImplementedError()
+
+		self.concrete_cal = concrete_cal
+		self.msg = u"文件夹内统计的总时长为:" + settings.LINESEP
+
+	def measure(self, intervals):
+		self.accum = self.add(self.accum, self.concrete_cal.measure(intervals).duration)
+		return self
+
+	def output_duration(self, fd=sys.stdout, unit='h'):
+		fd.write(self.msg.encode(settings.ENCODING))
+		self.concrete_cal.duration = self.accum
+		self.concrete_cal.output_duration(fd, unit)
--- textgrid/qualify_textgrid/tests/test_calculagraph.py
+++ textgrid/qualify_textgrid/tests/test_calculagraph.py
@@ -2,35 +2,12 @@
 import os
 import re
 import unittest
-from calculagraph import Calculagraph, CategoricalCalculagraph
+import StringIO
+import calculagraph
 
-
-class CalculagraphTestCase(unittest.TestCase):
+class BaseCalculagraphTestCase(unittest.TestCase):
 	def setUp(self):
-		self.intervals = [{"text":"1测试", "xmin":0.0, "xmax":2.0, "lineno":1},
-						  {"text":"", "xmin":2.0, "xmax":2.5, "lineno":2},
-						  {"text":"2测试", "xmin":2.5, "xmax":30.5, "lineno":3},
-						  {"text":"1", "xmin":4.5, "xmax":3.5, "lineno":4},
-						  ]
-
-		MATCH_PARSER = re.compile('.+', re.UNICODE)
-		self.cg = Calculagraph(MATCH_PARSER)
-
-	def test_measure(self):
-		duration = self.cg.measure(self.intervals)
-		self.assertEqual(duration, 30.0)
-		self.assertEqual(len(self.cg.errors), 1)
-
-	def test_prints(self):
-		self.cg.measure(self.intervals)
-		output = self.cg.output_duration()	# 's'
-		self.assertEqual(len(output), 1)
-		self.assertEqual(output[0], u"有效时长为30.000000秒")
-
-		output = self.cg.output_duration('m')
-		self.assertEqual(len(output), 1)
-		self.assertEqual(output[0], u"有效时长为0.500000分")
-
+		self.cg = calculagraph.BaseCalculagraph()	
 
 	def test_get_unit_display(self):
 		divider, unit_display = self.cg.get_unit_display('h')
@@ -40,6 +17,42 @@ class CalculagraphTestCase(unittest.TestCase):
 		with self.assertRaises(KeyError) as ke:
 			self.cg.get_unit_display('k')
 
+	def test_output(self):
+		output = StringIO.StringIO()
+		self.cg.output(output, 10)
+		self.assertEqual(output.getvalue(), u"10秒"+os.linesep)
+
+		output = StringIO.StringIO()
+		self.cg.output(output, 3600, msg=u"{extra_arg}测试{duration_display}{unit_display}", unit='h', extra_arg=u"输出")
+		self.assertEqual(output.getvalue(), u"输出测试1.0小时")
+
+class PatternCalculagraphTestCase(unittest.TestCase):
+	def setUp(self):
+		self.intervals = [{"text":"1测试", "xmin":0.0, "xmax":2.0, "lineno":1},
+						  {"text":"", "xmin":2.0, "xmax":2.5, "lineno":2},
+						  {"text":"2测试", "xmin":2.5, "xmax":30.5, "lineno":3},
+						  {"text":"1", "xmin":4.5, "xmax":3.5, "lineno":4},
+						  ]
+
+		MATCH_PARSER = re.compile('.+', re.UNICODE)
+		self.cg = calculagraph.PatternCalculagraph(MATCH_PARSER)
+
+	def test_measure(self):
+		cg = self.cg.measure(self.intervals)
+		self.assertEqual(cg.duration, 30.0)
+		self.assertEqual(len(cg.errors), 1)
+		self.assertEqual(cg.errors[0], u'错误: 在第4行检测到xmax的值大于xmin值')
+
+	def test_output_duration(self):
+		self.cg.measure(self.intervals)
+		
+		output = StringIO.StringIO()
+		self.cg.output_duration(output)	# 's'
+		self.assertEqual(output.getvalue(), u"文件有效时长为30.0秒"+os.linesep)
+
+		output = StringIO.StringIO()
+		self.cg.output_duration(output, 'm')
+		self.assertEqual(output.getvalue(), u"文件有效时长为0.5分"+os.linesep)
 
 
 class CategoricalCalculagraphTestCase(unittest.TestCase):
@@ -59,16 +72,95 @@ class CategoricalCalculagraphTestCase(unittest.TestCase):
 			'3': '3-',
 			'4': '4-'
 		}
-		self.cg = CategoricalCalculagraph(TEXT_CATEGORY_PARSER, TEXT_CATEGORY_KEY, MARKS_MEANING)
+		self.cg = calculagraph.CategoricalCalculagraph(TEXT_CATEGORY_PARSER, TEXT_CATEGORY_KEY, MARKS_MEANING)
 
 	def test_measure(self):
-		durations = self.cg.measure(self.intervals)
-		self.assertEqual(durations['1'], 4)
-		self.assertEqual(durations['2'], 0.5)
-		self.assertEqual(len(self.cg.errors), 1)
+		cg = self.cg.measure(self.intervals)
+		self.assertEqual(cg.duration['1'], 4)
+		self.assertEqual(cg.duration['2'], 0.5)
+		self.assertEqual(len(cg.errors), 1)
 
-	def test_prints(self):
+	def test_output_duration(self):
 		self.cg.measure(self.intervals)
-		output = self.cg.output_duration()
-		self.assertEqual(len(output), 2)
-		self.assertEqual(output[0], u"1-时长为4.000000秒")
+		
+		output = StringIO.StringIO()
+		self.cg.output_duration(output)
+		print_durations = filter(lambda x: x.strip(), output.getvalue().split(os.linesep))
+		self.assertEqual(len(print_durations), 2)
+		self.assertEqual(output.getvalue(), u"1-时长为4.0秒"+os.linesep+u"2-时长为0.5秒"+os.linesep)
+
+
+class OverallCalculagraphTestCase(unittest.TestCase):
+	def setUp(self):
+		self.intervals1 = [{"text":"1测试", "xmin":0.0, "xmax":2.0, "lineno":1},
+						  {"text":"", "xmin":2.0, "xmax":2.5, "lineno":2},
+						  {"text":"2测试", "xmin":2.5, "xmax":30.5, "lineno":3},
+						  {"text":"1", "xmin":4.5, "xmax":3.5, "lineno":4},
+						  ]
+
+		self.intervals2 = [{"text":"1测试", "xmin":0.0, "xmax":2.0, "lineno":1},
+						  {"text":"", "xmin":2.0, "xmax":3, "lineno":2},
+						  {"text":"3测试", "xmin":3, "xmax":30.5, "lineno":3},
+						  {"text":"测试", "xmin":30.5, "xmax":31, "lineno":4},
+
+						  ]
+
+	def test_dict_add(self):
+		d1 = {'a':1, 'b':2}
+		d2 = {'a':3, 'c':4}
+		self.assertEqual(calculagraph.dict_add(d1, d2), {'a':4, 'b':2, 'c':4})
+
+	def test_init_with_patterncal(self):
+		pcg = calculagraph.PatternCalculagraph(re.compile('.+', re.UNICODE))
+		self.ocg = calculagraph.OverallCalculagraph(pcg)
+		self.assertEqual(self.ocg.accum, 0)
+		self.ocg.measure(self.intervals1)
+		self.assertEqual(self.ocg.accum, 30)	# 2+28
+		self.ocg.measure(self.intervals2)
+		self.assertEqual(self.ocg.accum, 60)	# 30 + 2+27.5+0.5
+		output = StringIO.StringIO()
+		self.ocg.output_duration(output, unit='m')
+		self.assertEqual(output.getvalue(), u"文件夹内统计的总时长为:"+os.linesep+u"文件有效时长为1.0分"+os.linesep)
+
+		pcg = calculagraph.PatternCalculagraph()
+		self.ocg = calculagraph.OverallCalculagraph(pcg)
+		self.assertEqual(self.ocg.accum, 0)
+		self.ocg.measure(self.intervals1)
+		self.assertEqual(self.ocg.accum, 30.5)	# 2+0.5+28
+		self.ocg.measure(self.intervals2)
+		self.assertEqual(self.ocg.accum, 61.5)	# 30.5 + 2+1+27.5+0.5
+		self.ocg.accum = 5400
+		output = StringIO.StringIO()
+		self.ocg.output_duration(output)
+		self.assertEqual(output.getvalue(), u"文件夹内统计的总时长为:"+os.linesep+u"文件有效时长为1.5小时"+os.linesep)
+
+	def test_init_with_catcal(self):
+		TEXT_CATEGORY_PARSER = re.compile('^(?P<category>[1-4])\D.*', flags=re.UNICODE)
+		TEXT_CATEGORY_KEY = 'category'
+
+		MARKS_MEANING = {
+			'1': '1-',
+			'2': '2-',
+			'3': '3-',
+			'4': '4-'
+		}
+		ccg = calculagraph.CategoricalCalculagraph(TEXT_CATEGORY_PARSER, TEXT_CATEGORY_KEY, MARKS_MEANING)
+		self.ocg = calculagraph.OverallCalculagraph(ccg)
+		self.assertEqual(self.ocg.accum, {})
+		self.ocg.measure(self.intervals1)
+		self.assertEqual(self.ocg.accum['1'], 2)	# 2.0 - 0.0
+		self.assertEqual(self.ocg.accum['2'], 28)	# 30.5 - 2.5
+
+		self.ocg.measure(self.intervals2)
+		self.assertEqual(self.ocg.accum['1'], 4)	# 2 + 2.0-0.0
+		self.assertEqual(self.ocg.accum['2'], 28)	# 28
+		self.assertEqual(self.ocg.accum['3'], 27.5)		#30.5 - 3
+		output = StringIO.StringIO()
+		self.ocg.output_duration(output, unit='s')
+		print_durations = filter(lambda x: x.strip(), output.getvalue().split(os.linesep))
+		self.assertEqual(print_durations[0], u"文件夹内统计的总时长为:")
+		self.assertTrue(u"1-时长为4.0秒" in print_durations)
+		self.assertTrue(u"2-时长为28.0秒" in print_durations)
+		self.assertTrue(u"3-时长为27.5秒" in print_durations)
+
+
