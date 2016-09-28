0676fe14e99934a45b925f932ae6984d99304406
xiaoyang <muma.378@163.com>
2016-09-28 10:21:09 +0800

    [FIX] refactor collect.py

--- utils/collect.py
+++ utils/collect.py
@@ -12,17 +12,12 @@ def collect(filename):
 	with open(filename, 'r') as f:
 		for line in f:
 			collections.update(map(lambda x: x.replace('\\', ''), filter(lambda x: x.strip(), line.split('\t')[1:-1])))
-	import pdb;pdb.set_trace()
 	with open('uniq_'+filename, 'w') as f:
 		for chars in collections:
 			f.write(chars)
 			f.write('\n')
 
 
-def main(filename):
-	collect(filename)
-	
-
 if __name__ == '__main__':
 	filename = sys.argv[1]
-	main(filename)
+	collect(filename)
